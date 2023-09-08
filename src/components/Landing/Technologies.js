87dc054025e084d31ca666ffec4517b82ed44169
Ollie Beenham <73618201+ElBeenMachine@users.noreply.github.com>
2023-09-08 13:11:58 +0000

    Added technologies and footer

---
+++ src/components/Landing/Technologies.js
@@ -0,0 +1,18 @@
+import { Technologies } from "@/config";
+import TechCard from "../Cards/TechCard";
+
+function Technlolgies() {
+    const data = Technologies.sort((a, b) => a.name - b.name);
+    return (
+        <div className={"mb-16"}>
+            <h1 className={"text-4xl pb-5"}>My technologies</h1>
+            <div className={"flex justify-start flex-wrap"}>
+                {data.map((tech) => (
+                    <TechCard name={tech.name} image={tech.image} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Technlolgies;
