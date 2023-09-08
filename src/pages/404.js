55f2c0855b7155a88fb8333bb56c840ca5da9e86
Ollie Beenham <73618201+ElBeenMachine@users.noreply.github.com>
2023-09-08 13:12:52 +0000

    Added footer to error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Layout/Footer";
 import Layout from "@/components/Layout/Layout";
 import Navbar from "@/components/Layout/Navbar";
 
@@ -5,24 +6,27 @@ export default function Error404() {
     return (
         <Layout title={"404 | Not Found"}>
             <Navbar />
-            <div
-                className={
-                    "w-full h-[calc(100vh-80px)] flex justify-center items-center flex-col p-10 text-center"
-                }
-            >
-                <h1 className={"text-4xl font-semibold mb-7"}>Error 404</h1>
-                <p className={"max-w-xl mb-7"}>
-                    The page or resource that you are looking for either does
-                    not exist or has been moved to a new location.
-                </p>
-                <a
+            <div className={"mx-10 md:px-6"}>
+                <div
                     className={
-                        "block w-max bg-my-cyan-400 hover:bg-my-cyan-300 transition-all px-5 py-3 rounded-md"
+                        "w-full h-[calc(100vh-80px)] flex justify-center items-center flex-col p-10 text-center"
                     }
-                    href={"/"}
                 >
-                    Go Home
-                </a>
+                    <h1 className={"text-4xl font-semibold mb-7"}>Error 404</h1>
+                    <p className={"max-w-xl mb-7"}>
+                        The page or resource that you are looking for either
+                        does not exist or has been moved to a new location.
+                    </p>
+                    <a
+                        className={
+                            "block w-max bg-my-cyan-400 hover:bg-my-cyan-300 transition-all px-5 py-3 rounded-md"
+                        }
+                        href={"/"}
+                    >
+                        Go Home
+                    </a>
+                </div>
+                <Footer />
             </div>
         </Layout>
     );
