0a4f72c7567c211d64deb3b59df3b8e73e23ee5a
Ollie Beenham <73618201+ElBeenMachine@users.noreply.github.com>
2023-09-08 18:56:26 +0000

    SEO Optimisation

--- src/pages/api/getMeta.js
+++ src/pages/api/getMeta.js
@@ -2,20 +2,20 @@ import { parser } from "html-metadata-parser";
 
 export default async function (req, res) {
     if (!req?.query?.url) {
-        res.status(200).redirect("/img/metadata/no-meta.jpg");
+        res.status(200).redirect("/img/metadata/no-meta.webp");
         return;
     }
 
     try {
         const metadata = await parser(req.query.url);
         if (!metadata?.og?.image) {
-            res.status(200).redirect("/img/metadata/no-meta.jpg");
+            res.status(200).redirect("/img/metadata/no-meta.webp");
             return;
         }
         res.status(200).redirect(metadata.og.image);
         return;
     } catch (error) {
-        res.status(200).redirect("/img/metadata/no-meta.jpg");
+        res.status(200).redirect("/img/metadata/no-meta.webp");
         return;
     }
 }
