49bbe277fef90ce7276670a5263ac6ce31d949ae
Ollie Beenham <73618201+ElBeenMachine@users.noreply.github.com>
2023-10-07 14:29:49 +0000

    some styling fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,25 +7,25 @@ function Hero() {
         >
             <h3
                 className={"text-[#00ffff] font-bold text-lg mb-8 big-header"}
-                style={{ animationDelay: "4100ms" }}
+                style={{ animationDelay: "3600ms" }}
             >
                 Hi, my name is
             </h3>
             <h1
                 className={"text-gray-100 font-black text-5xl mb-2 big-header"}
-                style={{ animationDelay: "4200ms" }}
+                style={{ animationDelay: "3700ms" }}
             >
                 Ollie Beenham.
             </h1>
             <h2
                 className={"text-gray-300 font-black text-5xl mb-8 big-header"}
-                style={{ animationDelay: "4300ms" }}
+                style={{ animationDelay: "3800ms" }}
             >
                 I build things for the web.
             </h2>
             <p
                 className={"max-w-md text-gray-300 mb-8 big-header"}
-                style={{ animationDelay: "4400ms" }}
+                style={{ animationDelay: "3900ms" }}
             >
                 I am a university studying software engineering. I am currently
                 working on creating experiences and automations with a
@@ -39,7 +39,7 @@ function Hero() {
                 className={
                     "transition-all text-[#00ffff] font-semibold border rounded-md border-[#00ffff] px-8 py-4 hover:text-[#303030] hover:bg-[#00ffff] big-header"
                 }
-                style={{ animationDelay: "4500ms" }}
+                style={{ animationDelay: "4000ms" }}
             >
                 Check Out My GitHub!
             </button>
